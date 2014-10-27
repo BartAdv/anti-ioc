d0821dfa665fed09f985e39151cf6df9c1b01e2d
BartAdv <bprzygoda@gmail.com>
2014-10-27 23:27:37 +0000

    Working in event->command mode rather than updating environment, that is, whole world

--- src/Free.hs
+++ src/Free.hs
@@ -5,78 +5,72 @@ module Free where
 import Prelude hiding (interact)
 import Data.String
 import Control.Monad
+import Control.Monad.Identity
 import Control.Monad.Free
+import qualified Control.Monad.State as ST
 import Data.Dynamic
 import Renderer hiding (draw, redraw)
 import qualified Renderer
 import Entities hiding (add, get, update, put)
 import qualified Entities
 
-data Env = Env { renderer :: RenderingEnv
-               , entities :: Entities.EntitiesEnv }
-               deriving Show
+import Debug.Trace
 
 data Event = Collision Dynamic
+           | Rendered RenderHandle
            | Key Char
            | Damage Int
+           deriving Show
+                      
+data Command = Render RenderInfo
+             | Redraw RenderHandle (RenderInfo -> RenderInfo)
+             | Clear RenderHandle
+               
+instance Show Command where
+  show (Render ri) = "render: " ++ (show ri)
+  show (Redraw rh _) = "redraw " ++ (show rh)
+  show (Clear rh) = "clear: " ++ (show rh)
 
-data Step next =
-    Interact (Event -> next)
-  | GetEnv (Env -> next)
-  | PutEnv Env (() -> next)
-  | Get (Dynamic -> next)
-  | Put Dynamic (() -> next)
-  | Error String
-  deriving Functor
+data Step next = Await (Event -> next)
+               | Order Command (() -> next)
+               | Get (Dynamic -> next)
+               | Put Dynamic (() -> next)
+               | Error String
+               deriving Functor
 
 instance Show (Step a) where
-  show (Interact g) = "Interact"
-  show (GetEnv g) = "Get"
-  show (PutEnv e g) = "Put " ++ show e
+  show (Await g) = "Await"
+  show (Order c g) = "Order: " ++ (show c)
   show (Get g) = "Get"
   show (Put e g) = "Put"
   show (Error s) = "Error: " ++ s
 
-type Program = Free Step 
+type Program = Free Step
 
-interp :: Env -> Dynamic -> [Event] -> Program a -> Either (Env, Dynamic, Program a) String
-interp env ent events prog =
-  case (prog, events) of
-    (Impure (Interact g), e:es) -> interp env ent es (g e)
-    (Impure (GetEnv g), _)      -> interp env ent events (g env)
-    (Impure (PutEnv env' g), _) -> interp env' ent events (g ())
-    (Impure (Get g), _)         -> interp env ent events (g ent)
-    (Impure (Put d g), _)       -> interp env d events (g ())
-    (Impure (Error s), _)       -> Right s
-    (Pure r, _)               -> Left (env, ent, return r)
-    otherwise                 -> Left (env, ent, prog)
--- ^ don't really like the way its pattern-matched
+interp :: Program a -> [Event] -> Dynamic -> Either (Program a, [Command], Dynamic) String
+interp = interp' []
+  where
+    interp' cs (Impure (Await g))   (e:es) ent = interp' cs (g e) es ent
+    interp' cs (Impure (Order c g)) es ent = interp' (c:cs) (g ()) es ent
+    interp' cs (Impure (Get g))     es     ent = interp' cs (g ent) es ent
+    interp' cs (Impure (Put d g))   es     ent = interp' cs (g ()) es d
+    interp' cs (Impure (Error s))   _      _   = Right s
+    interp' cs (Pure r)             _      ent = Left (return r, cs, ent)
+    interp' cs prog                 _      ent = Left (prog, cs, ent)
+
+printp :: Program a -> String
+printp (Impure s) = show s
+printp (Pure _) = "pure"
 
     -- hmm
 liftF :: (Functor f) => f r -> Free f r
 liftF command = Impure (fmap Pure command)
 
-interact :: Program Event
-interact = liftF (Interact id)
+await :: Program Event
+await = liftF (Await id)
 
-collide :: Program Dynamic
-collide = do
-  e <- interact
-  case e of
-    Collision d -> return d
-    otherwise -> collide
-
-waitForKey :: Program Char
-waitForKey = do
-  e <- interact
-  case e of
-    Key c -> return c
-    otherwise -> waitForKey
--- ^^ some common pattern with not that straightforward wy to be refactored
--- out
-
-failure :: String -> Program a
-failure s = liftF $ Error s
+order :: Command -> Program ()
+order cmd = liftF $ Order cmd id
 
 get :: Program Dynamic
 get = liftF $ Get id
@@ -84,69 +78,45 @@ get = liftF $ Get id
 put :: Typeable a => a -> Program ()
 put e = liftF $ Put d id
   where d = toDyn e
+        
+failure :: String -> Program a
+failure s = liftF $ Error s
 
-getEnv :: Program Env
-getEnv = liftF $ GetEnv id
+collide :: Program Dynamic
+collide = do
+  e <- await
+  case e of
+    Collision d -> return d
+    otherwise -> collide
 
-putEnv :: Env -> Program ()
-putEnv env = liftF $ PutEnv env id
-
-getEntity :: EntityHandle -> Program (Maybe Dynamic)
-getEntity h = do
-  env <- getEnv
-  return $ Entities.get (entities env) h
-
-addEntity :: Dynamic -> Program EntityHandle
-addEntity d = do
-  env <- getEnv
-  let (e', h) = Entities.add (entities env) d
-  putEnv env { entities = e' }
-  return h
-
-updateEntity :: EntityHandle -> Dynamic -> Program ()
-updateEntity h d = do
-  env <- getEnv
-  let e' = Entities.put (entities env) h (Just d)
-  putEnv env { entities = e' }
-
-removeEntity :: EntityHandle -> Program ()
-removeEntity h = do
-  env <- getEnv
-  let e' = Entities.put (entities env) h Nothing
-  putEnv env { entities = e' }
-
+waitForKey :: Program Char
+waitForKey = do
+  e <- await
+  case e of
+    Key c -> return c
+    otherwise -> waitForKey
+-- ^^ some common pattern with not that straightforward wy to be refactored
+-- out
+    
 draw :: Int -> Int -> Char -> Program RenderHandle
 draw x y c = do
-  env <- getEnv
-  let (re, rh) = Renderer.draw (renderer env) RenderInfo { posX = x, posY = y, sign = c }
-  putEnv env { renderer = re }
-  return rh
+  order $ Render RenderInfo { posX = x, posY = y, sign = c }
+  loop
+    where loop = do
+            ev <- await
+            case trace (show ev) $ ev of
+              Rendered rh -> return rh
+              _ -> loop -- notice that we effectively ditch every event till we got what we wanted, this could be changed
 
 redraw :: RenderHandle -> (RenderInfo -> RenderInfo) -> Program ()
-redraw h upd = do
-  env <- getEnv
-  let re = Renderer.redraw (renderer env) h (\ri -> Just $ upd ri)
-  putEnv env { renderer = re }
+redraw h upd = order $ Redraw h upd
 
 clear :: RenderHandle -> Program ()
-clear h = do
-  env <- getEnv
-  putEnv env { renderer = Renderer.redraw (renderer env) h (const Nothing) }
+clear h = order $ Clear h
 
 {-
 - usage
 -}
-
--- this is just program for entities that 'die' on a collision
-prog :: Program a
-prog = do
-  rh <- draw 1 1 '*'
-  forever $ do
-    d <- collide
-    when (fromDyn d "" == "a") $ do
-      redraw rh (\ri -> ri { sign = 'x' })
-      put $ toDyn "I'm dead"
-      return ()
 
 data Player = Player { health :: Int 
                      , playerX :: Int
@@ -166,7 +136,7 @@ player :: Program a
 player = do
   rh <- with $ \Player { playerX = x, playerY = y } -> draw x y '@'
   forever $ do
-    e <- interact
+    e <- await
     whenEntity (\p -> (health p) > 0) $ do
       case e of
         Damage dmg -> do with $ \p -> put p { health = (health p) - dmg }
@@ -195,49 +165,29 @@ prog2 = do
     d <- collide
     put d
 
-emptyEnv = Env { renderer = Renderer.init, entities = Entities.init }
 entity = toDyn "Whatever"
 playerEntity = toDyn Player { playerX = 0, playerY = 0, health = 100 }
 
--- prog succesfuly executed
-Left (env, e, r) = interp emptyEnv entity [Collision $ toDyn "a"] prog
---
--- unchanged
-Left (env', e', r') = interp emptyEnv entity [ Collision $ toDyn "w"
-              , Collision $ toDyn "b"] prog
+test :: [Event] -> ST.StateT (Program a, Dynamic) IO ()
+test es = do
+  (prog, ent) <- ST.get
+  let Left (prog', cs, ent') = interp prog es ent
+      sh = fromDyn ent' Player{}
+  ST.liftIO $ putStrLn $ (printp prog') ++ "\n" ++ (show cs) ++ "\n" ++ (show sh)
+  ST.liftIO $ putStrLn ""
+  ST.put (prog', ent')
 
--- check if we'rfe eating events correctly
-Left (env'', e'', r'') = interp emptyEnv entity [ Collision $ toDyn "brelam"
-                               , Key 'a'
-                               , Collision $ toDyn "a" ] prog
+tests :: ST.StateT (Program a, Dynamic) IO ()
+tests = do
+  test []
+  test [Rendered 1]
+  test [Key 'j']
+  test [Key 'j']
+  return ()
 
-Left (env''', e''', r''') = interp emptyEnv entity [ Collision $ toDyn "brelam"
-                                 , Collision $ toDyn "a" ] prog
+main :: IO ()
+main = do
+       _ <- ST.runStateT tests (player, playerEntity)
+       return ()
 
-Left (_, unchangedPos, _) = interp emptyEnv playerEntity [ Key 'j', Damage 10, Key 'k' ] player
-p' = fromDyn unchangedPos Player {}
-
-Left (_, changedPos, _) = interp emptyEnv playerEntity [ Key 'j', Damage 101, Key 'k' ] player
-p'' = fromDyn changedPos Player {}
-
--- sequence
-seqTest :: Program a
-seqTest = forever $ do
-  e <- interact
-  case e of
-    Key _ -> do
-      e' <- interact
-      case e' of
-        Damage dmg -> do with $ \p -> put p { health = (health p) - dmg }
-        _ -> return ()
-    _ -> return ()
-
--- ohh
-Left (_, sq', seqTest') = interp emptyEnv playerEntity [ Damage 10 ] seqTest
-sqTest = fromDyn sq' Player {}
-Left (_, sq'', seqTest'') = interp emptyEnv sq' [ Damage 10 ] seqTest'
-sqTest' = fromDyn sq'' Player {}
-Left (_, sq''', seqTest''') = interp emptyEnv sq'' [ Key 'a' ] seqTest''
-sqTest'' = fromDyn sq' Player {}
-Left (_, sq'''', seqTest'''') = interp emptyEnv sq''' [ Damage 10 ] seqTest'''
-sqTest''' = fromDyn sq'''' Player {}
+  
